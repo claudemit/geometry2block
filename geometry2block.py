f5d6e24cb7d06bb28106cd9f4a5cb36f9720bf6e
claudemit <claudemit13@gmail.com>
2015-10-01 18:16:01 +0800

    Update geometry2block.py

--- geometry2block.py
+++ geometry2block.py
@@ -5,6 +5,7 @@ written by claudemit
 2015-09-26
 """
 from time import clock
+import Rhino
 import rhinoscriptsyntax as rs
 import System
 
@@ -12,24 +13,24 @@ def cp_filt(rhino_object, geometry, component_index):
     return rs.IsBrep(geometry) | rs.IsMesh(geometry)
 
 def Geo2Block():
+    Rhino.RhinoApp.RunScript("_Purge Enter",False)
     #属性--设置图层
     layName= rs.GetString("Add a New Layer to put Blocks")
     if layName in rs.LayerNames():
         rs.MessageBox("This LayerName already exist!")
         return
-    rs.AddLayer(layName,System.Drawing.Color.Gold)
-    preLayer= rs.CurrentLayer(layName)
+    layId= rs.AddLayer(layName,System.Drawing.Color.Gold)
+    preLayer= rs.CurrentLayer(layId)
     
-    #选取几何体 
-    go = rs.GetObjects("Select Geometry",custom_filter= cp_filt)
-    ObjRefs = go
+    #选取几何体     
+    ObjIds= rs.GetObjects("Select Geometry",custom_filter= cp_filt)
     
     #生成图块
     start= clock()
     basepoint = rs.AddPoint(0,0,0)
-    if(ObjRefs):        
+    if(ObjIds):        
         i= 0
-        for obj in ObjRefs:
+        for obj in ObjIds:
             name = 'block_%s' %i
             if rs.IsBlock(name):
                 rs.MessageBox("Block Definition"+ name+"Already Exists")
@@ -37,13 +38,13 @@ def Geo2Block():
             block= rs.AddBlock([obj], basepoint, name, False)            
             rs.ObjectLayer(rs.InsertBlock(block, basepoint), layName)
             i= i+1
-#        rs.MessageBox("IN TOTAL %d BLOCKS" % i)
-    else:
-        rs.CurrentLayer(preLayer)
-        rs.DeleteLayer(layName)
-        rs.MessageBox("No Geometry Selected!")
-        return
-    print clock()-start
+        print clock()-start
+        rs.MessageBox("IN TOTAL %d BLOCKS" % i)        
+    else:        
+        rs.DeleteLayer(layId)
+        rs.MessageBox("No Geometry Selected!")   
+    rs.CurrentLayer(preLayer)
+    rs.DeleteObject(basepoint)
     
 if( __name__ == "__main__" ):
   #call function defined above
